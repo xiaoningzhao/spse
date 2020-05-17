888f97d34c6249c130d0819afb1c3142813104da
Xiaoning Zhao <zhaoxna@gmail.com>
2020-05-16 22:45:39 -0700

    Update strategy mapping

--- database.sql
+++ database.sql
@@ -26,30 +26,19 @@ CREATE TABLE strategy (
     PRIMARY KEY (strategy, ticker)
 );
 
-INSERT INTO `spse`.`user` (`password`, `username`) VALUES ('698d51a19d8a121ce581499d7b701668', 'Bob');
-INSERT INTO `spse`.`user` (`password`, `username`) VALUES ('698d51a19d8a121ce581499d7b701668', 'Sam');
-
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Ethical', 'AAPL', 'Apple', 80);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Ethical', 'ADBE', 'Adobe', 70);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Ethical', 'NSRGY', 'Nestle', 67);
-
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Growth', 'VTI', 'Vanguard Total Stock Market ETF', 84);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Growth', 'IXUS', 'iShares Core MSCI Total Intl Stk', 75);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Growth', 'ILTB', 'iShares Core 10+ Year USD Bond', 69);
-
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Index', 'VTI', 'Vanguard Total Stock Market ETF', 84);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Index', 'IXUS', 'iShares Core MSCI Total Intl Stk', 75);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Index', 'ILTB', 'iShares Core 10+ Year USD Bond', 69);
-
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Quality', 'VTI', 'Vanguard Total Stock Market ETF', 84);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Quality', 'IXUS', 'iShares Core MSCI Total Intl Stk', 75);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Quality', 'ILTB', 'iShares Core 10+ Year USD Bond', 69);
-
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Value', 'VTI', 'Vanguard Total Stock Market ETF', 84);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Value', 'IXUS', 'iShares Core MSCI Total Intl Stk', 75);
-INSERT INTO `spse`.`strategy` (`strategy`, `ticker`, `ticker_name`, `rating`) VALUES ('Value', 'ILTB', 'iShares Core 10+ Year USD Bond', 69);
-
-INSERT INTO `spse`.`portfolio` (`user_id`, `ticker`, `ticker_name`, `share`, `purchase_price`) VALUES ('1', 'VMW', 'Wmware', '10', '100.23');
-INSERT INTO `spse`.`portfolio` (`user_id`, `ticker`, `ticker_name`, `share`, `purchase_price`) VALUES ('1', 'AAPL', 'Apple', '12', '221.56');
-INSERT INTO `spse`.`portfolio` (`user_id`, `ticker`, `ticker_name`, `share`, `purchase_price`) VALUES ('2', 'ADBE', 'Adobe', '8', '30.56');
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Ethical','AAPL','Apple',80);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Ethical','ADBE','Adobe',70);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Ethical','NSRGY','Nestle',67);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Growth','ATVI','Activision Blizzard',90);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Growth','FB','Facebook',65);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Growth','WORK','Slack Technologies',97);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Index','ILTB','iShares Core 10+ Year USD Bond',69);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Index','IXUS','iShares Core MSCI Total Intl Stk',75);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Index','VTI','Vanguard Total Stock Market ETF',84);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Quality','KRTX','Karuna Therapeutics Inc',47);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Quality','MSFT','Microsoft',88);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Quality','PLMR','Palomar Holdings Inc',70);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Value','GE','General Electric',79);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Value','T','ATT',62);
+INSERT INTO `spse`.`strategy` (`strategy`,`ticker`,`ticker_name`,`rating`) VALUES ('Value','XOM','Exxon Mobil Corporation',91);
 
